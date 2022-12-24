1ee63286451660de35dda60be637d046fc8e4d1c
DaJakeDev (JDC) <95346044+JDC-Unity@users.noreply.github.com>
2022-12-24 16:00:02 +1100

    added single download with specific path and fixed bugs

--- AdvRequest.cs
+++ AdvRequest.cs
@@ -10,11 +10,11 @@ namespace AdvancedDownload
 {
     public class AdvRequest
     {
-        
+        //1.7
         public delegate void downloadingHandler();
         public event downloadingHandler OnDownload = null;
         internal float progress = 0.0f;
-        internal string[] _links;
+        internal string[] _links = { "" };
         internal bool _downloading = false;
         internal string _path = Application.dataPath;
         internal string _root = null; //https://somethin.com/ - ROOT, then it cut by link https://somethin.com/folder/file.txt and result will be like this: 1- create folder 'folder' and put file.txt in this
@@ -22,6 +22,11 @@ namespace AdvancedDownload
         internal string timePassed;
         DownloadHandlerFile loader;
         public AdvRequest() { }
+        public AdvRequest(string url, string path)
+        {
+            _links[0] = url;
+            _path = path;
+        }
         public AdvRequest(IEnumerable<string> urls, string root) // with root it will cut by /folder/file.som
         {
             List<string> converting = new List<string>();
@@ -35,7 +40,7 @@ namespace AdvancedDownload
             foreach (string i in urls) { converting.Add(i); }
             _links = converting.ToArray();
         }
-        private void InvokeEvent() { if (OnDownload!=null) { OnDownload.Invoke(); } }
+        private void InvokeEvent() { if (OnDownload != null) { OnDownload.Invoke(); } }
         async public void Begin()
         {
             _downloading = true;
@@ -65,7 +70,7 @@ namespace AdvancedDownload
                         }
                     }
                 }
-                    
+
                 loader = new DownloadHandlerFile(pathtodo + "/" + Path.GetFileName(_links[index])); loader.removeFileOnAbort = true;
                 r = new UnityWebRequest(_links[index]); r.downloadHandler = loader; r.disposeDownloadHandlerOnDispose = true;
                 op = r.SendWebRequest();
@@ -84,7 +89,7 @@ namespace AdvancedDownload
                 stopWatch.Stop();
                 timePassed = String.Format("{0:00m}:{1:00s}.{2:00ms}", stopWatch.Elapsed.Minutes, stopWatch.Elapsed.Seconds, stopWatch.Elapsed.Milliseconds / 10);
                 InvokeEvent();
-                }
+            }
             _downloading = false;
         }
 
